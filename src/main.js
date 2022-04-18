5e0712833ccea14cf0629489cce6ddb70a1826fc
binaryArrow <cancihan96@gmail.com>
2022-04-19 00:13:23 +0200

    add: convert json to atlas object TODO: create Atlas, ref code

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ const {app, BrowserWindow, dialog} = require('electron')
 
 const ipcMain = require('electron').ipcMain
 const fs = require('fs')
-const {AtlasModel} = require("./models/AtlasModel");
-
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
@@ -33,7 +31,31 @@ function createAtlasFromJson(filePath){
         if(err) throw err
         const path = require('path')
         const name = path.basename(filePath)
+        const nameForAtlas = name.replace(".json", ".png")
+        const nameWithAse = name.replace(".json", ".ase")
         const file = JSON.parse(data.toString())
+        console.log(file)
+        const atlas = {
+            name: nameForAtlas,
+            size: `${file.frames[nameWithAse].frame.w}, ${file.frames[nameWithAse].frame.h}`,
+            format: file.meta.format,
+            tiles: []
+        }
+        file.meta.slices.forEach(entry => {
+            atlas.tiles.push({
+                name: entry.name,
+                rotate: 'false',
+                xy: `${entry.keys[0].bounds.x}, ${entry.keys[0].bounds.y}`,
+                size: `${entry.keys[0].bounds.w}, ${entry.keys[0].bounds.h}`,
+                orig: `${entry.keys[0].bounds.w}, ${entry.keys[0].bounds.h}`,
+                offset: '0, 0',
+                index: '-1'
+
+            })
+        })
+
+        console.log(atlas)
+
     })
 }
 app.whenReady().then(()=>{
